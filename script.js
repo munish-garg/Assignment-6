78acbf0d3046e5871ee863493fbd6e8acf50bb50
munish-garg <munishkumar123264@gmail.com>
2021-03-29 16:35:53 +0530

    handledEnterAndClickFunctionality

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ console.log(input);
 function addEvent(){
     //Here, I have stored the input value inside of the text variable..
     let text = input.value;
-    let ele = document.createElement('li');
-    ele.innerText = text;
-    list.append(ele);
-    input.value = '';
+    if(text !== ''){
+        let ele = document.createElement('li');
+        ele.innerText = text;
+        list.append(ele);
+        input.value = '';
+    }
+}
+
+function keyPressed(e){
+    if(e.key === 'Enter' && input.value !== ''){
+        //It means we have to add that event to the list...
+        let ele = document.createElement('li');
+        ele.innerText = input.value;
+        list.append(ele);
+        input.value = '';
+
+    }
 }
 
 btn.addEventListener('click', addEvent);
+input.addEventListener('keydown', keyPressed);
